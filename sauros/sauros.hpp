dabef38ae02dca50da93ccf07daa335b0b6af445
bosley <bosley117@gmail.com>
2023-03-07 17:05:00 -0500

    migration

---
+++ sauros/sauros.hpp
@@ -0,0 +1,16 @@
+#ifndef SAUROS_HPP
+#define SAUROS_HPP
+
+#include "cell.hpp"
+#include "driver.hpp"
+#include "environment.hpp"
+#include "exceptions.hpp"
+#include "format.hpp"
+#include "front/parser.hpp"
+#include "processor/processor.hpp"
+#include "rang.hpp"
+#include "system/system.hpp"
+#include "types.hpp"
+#include "version.hpp"
+
+#endif
